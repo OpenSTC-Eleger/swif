bb4c5c8d161614aee6e3ca3c62666da899f42879
siclic <pierreyves.fadet@siclic.fr>
2012-11-20 20:55:27 +0100

    merge

--- js/views/EventView.js
+++ js/views/EventView.js
@@ -1,43 +1,101 @@
 openstm.Views.EventView = Backbone.View.extend({
 	  
-		el : '#myModal',
+	
+		templateHTML: 'tooltip',
 
-		templateHTML: 'task_duration',
-		
-        events : {
-          "click .btn-primary" : "save"
-        },
 
        
         initialize: function() {
             _.bindAll(this);           
         },
-        render: function() {
-        	$(this.el).modal();
+        
+        render: function(event) {
+        	this.event = event;
+        	var self= this;
+        	
+	        $.get("templates/" + this.templateHTML + ".html", function(templateData){
+		  
+				var template = _.template(templateData, {
+					lang: openstm.lang,
+	
+				});
+			
+				$(self.el).html(template);
+				self.template = template;
+				event.qtip({ 
+
+	        		content: {    
+		            	title: { text: self.model.get('name'),button: true, },            	
+		            	text: self.template           	
+	        		},
+					   events: {
+	        			 render: function(event, api) {
+	        			   $('form', this).bind('submit', function(event) {
+	        				   
+	        				   	params = { 
+							               id: self.model.get('id'),
+							    		   state: $('#eventState').is(':checked')?"done":self.model.get('state'),
+							               total_hours: $('#eventTimeSpent').val(),
+							               remaining_hours: $('#eventTimeRemaining').val(),
+							    };
+							    self.save(params);  
+							    $(this).destroy();							    
+	        					event.preventDefault();
+	        				 });
+	        			}					      
+					 },
+	                position: {
+	                    at: 'top center',
+	                    // Position the tooltip above the link
+	                    my: 'bottom center',
+	                    adjust: {
+	                        y: -2,
+	                        resize: false // We'll handle it manually
+	                    },
+	                    viewport: self.el,
+	                    container: self.el
+	                },
+	                show: {
+	                    ready: true,
+	                    event: false,
+	               	 	//event: "submit form",
+	                    //effect: self.save(),
+	                    modal: {
+	                        // 'true' = Make it modal (darken the rest of the page)...
+	                        on: false,                                
+	                        blur: false // ... but don't close the tooltip when clicked
+	                    }
+	                },
+	                hide: 'unfocus',
+	                style: {
+	                    classes: 'daytooltip ui-tooltip-dark ui-tooltip-shadow ui-tooltip-default width400',
+	                    tip: { width: 20, height: 8 }                                                 
+	                },	
+				});
+	
+			});	
+		
+			//$(this.el).hide().fadeIn('slow');
+	        return this;
         },        
         open: function() {
-        	this.$('#total_hours').val(this.model.get('total_hours'));
-            this.$('#remaining_hours').val(this.model.get('remaining_hours'));                        
+        	//this.$('#total_hours').val(this.model.get('total_hours'));
+            //this.$('#remaining_hours').val(this.model.get('remaining_hours'));                        
         },        
-        save: function() {
-            //this.model.set({'total_hours': this.$('#total_hours').val(), 'remaining_hours': this.$('#remaining_hours').val()});
-            
-            data = {'total_hours': this.$('#total_hours').val(), 
-                      'remaining_hours': this.$('#remaining_hours').val(),
-                      'id': this.model.attributes.id
-                      };
+        save: function(params) {
 
             if (this.model.isNew()) {
                 this.collection.create(this.model, {success: this.close});
             } else {
-                this.model.save(data,{success: this.close},false);
+                this.model.save(params,{success: this.close},false);
             }
-            $("#myModal").modal('hide');
+            $(this.event).css('border-color', 'red');
+            //$("#myModal").modal('hide');
         },
         close: function() {
-            this.el.dialog('close');
+            //this.el.dialog('close');
         },
         destroy: function() {
-            this.model.destroy({success: this.close});
+            //this.model.destroy({success: this.close});
         }        
     });
