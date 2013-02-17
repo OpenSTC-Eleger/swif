b5f35e27460dbff9c9813101fa6884519aa1562e
pierre-yves <pierreyves.fadet@free.fr>
2013-02-17 12:21:41 +0100

    before 18

--- js/app.js
+++ js/app.js
@@ -8,7 +8,7 @@ var app = {
 
     // Global variables //
     appVersion      : '0.1 alpha',
-    userBDD         : 'pontlabbe2',
+    userBDD         : 'pontlabbe3',
     uniq_id_counter : 0,	
     
     urlOE                       : 'http://octm-dev.siclic.fr:8069',
