191c5f37378ce41aaa4f1617c6f45488b5313023
siclic <siclic@siclic-dm4.(none)>
2013-04-15 23:50:13 +0200

    Bug planning découpe de tâches

--- js/views/EventsView.js
+++ js/views/EventsView.js
@@ -374,7 +374,7 @@ app.Views.EventsView = Backbone.View.extend({
 						if(!e.start) return false;
 						var eventDate = moment( e.start );
 						var currentDate = moment( date );
-						return ( currentDate.diff(eventDate, 'days', true)==0 || eventDate.diff(currentDate, 'days', true)==0 )
+						return ( currentDate.diff(eventDate, 'days', true)>-1 && currentDate.diff(eventDate, 'days', true)<1 )
 						
 					});
 					self.calculTask(copiedEventObject);
