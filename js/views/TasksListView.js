ad671f37b40c672821127ea091d66003c897b3e5
Simon MAHE <simon.mahe@siclic.fr>
2013-04-23 12:27:31 +0200

    Résolution Bug #638 Saisie d'un nombre decimal. Affichage du chef d'équipe dans les popover du planning #657

--- js/views/TasksListView.js
+++ js/views/TasksListView.js
@@ -362,18 +362,6 @@ app.Views.TasksListView = Backbone.View.extend({
 				}
 			});	
 			
-//			$('#modalTaskDone .modal-body').css("height", "550px");
-//			$('#modalAddTask').css("height", "800px");
-//			$('#modalAddTask .modal').css("height", "800px");
-//			$('#modalTimeSpent .modal-body').css("height", "550px");
-			
-			//$('#duallistbox_demo1_helper1').bootstrapDualListbox();
-			
-//			$('.modal').validate( {
-//				rules: {
-//					number: { required: true, min: 1 }
-//				}
-//			} );
 
 			$('.timepicker-default').timepicker({ showMeridian: false, disableFocus: true, showInputs: false, modalBackdrop: false});
 			$('*[data-toggle="tooltip"]').tooltip();
@@ -470,6 +458,8 @@ app.Views.TasksListView = Backbone.View.extend({
 		this.model = app.collections.tasks.get(this.pos);
     },
     
+    
+
     resetModal: function() {    	
     	$('.taskInput').val('');
     	$('.taskSelect').val(0);
@@ -479,8 +469,10 @@ app.Views.TasksListView = Backbone.View.extend({
     	
     },
     
+
+
 	/** Display the form to add a new Task
-		*/
+	*/
 	displayModalAddTask: function(e){
 			
     	this.resetModal();
@@ -513,6 +505,7 @@ app.Views.TasksListView = Backbone.View.extend({
 	},
 	
 	
+
 	fillDropdownEquipment: function(e){
 		e.preventDefault();
 		var target = $(e.target).attr('value');
@@ -526,6 +519,7 @@ app.Views.TasksListView = Backbone.View.extend({
 		}
 	},
 	
+
 
 	/** Save the Task
 	*/
@@ -558,28 +552,28 @@ app.Views.TasksListView = Backbone.View.extend({
 	    	 }
 	     }
 	     
-	     this.vehicule = input_equipment_id;
-	     this.equipments = _.map($("#equipmentsAdd").sortable('toArray'), function(equipment){ return _(_(equipment).strRightBack('_')).toNumber(); }); 
-	     this.equipments.push( input_equipment_id );
+		this.vehicule = input_equipment_id;
+		this.equipments = _.map($("#equipmentsAdd").sortable('toArray'), function(equipment){ return _(_(equipment).strRightBack('_')).toNumber(); }); 
+		this.equipments.push( input_equipment_id );
 
+	    
 	     
-	     
-	     var params = {
-	         user_id:  app.models.user.getUID(),
-	         date_start: mNewDateStart.toDate(),
-	         date_end: mNewDateEnd.toDate(),
-	         state: app.Models.Task.state[1].value,
-	         //equipment_id: input_equipment_id,
-	         equipment_ids: [[6, 0, this.equipments]],
-	         name: this.$('#taskName').val(),
-	         km: this.$('#equipmentKmAdd').val(),
-	         oil_qtity: this.$('#equipmentOilQtityAdd').val(),
-	         oil_price: this.$('#equipmentOilPriceAdd').val(),
-	         category_id: input_category_id,	         
-		     planned_hours: planned_hours,
-		     effective_hours: planned_hours,
-		     remaining_hours: 0,
-	     };
+		var params = {
+			user_id:  app.models.user.getUID(),
+			date_start: mNewDateStart.toDate(),
+			date_end: mNewDateEnd.toDate(),
+			state: app.Models.Task.state[1].value,
+			//equipment_id: input_equipment_id,
+			equipment_ids: [[6, 0, this.equipments]],
+			name: this.$('#taskName').val(),
+			km: this.$('#equipmentKmAdd').val(),
+			oil_qtity: this.$('#equipmentOilQtityAdd').val().replace(',', '.'),
+			oil_price: this.$('#equipmentOilPriceAdd').val().replace(',', '.'),
+			category_id: input_category_id,	         
+			planned_hours: planned_hours,
+			effective_hours: planned_hours,
+			remaining_hours: 0,
+		};
 	     this.saveNewStateTask(params, null, $('#modalAddTask'), null , true);
 	    // app.models.task.save(0,params,$('#modalAddTask'), null, "taches");
 	     //this.saveEquipment(  this.$('#equipmentKmAdd').val(), input_equipment_id)
@@ -664,9 +658,9 @@ app.Views.TasksListView = Backbone.View.extend({
     				taskParams = {
 						//equipment_id: input_equipment_id,
 						equipment_ids: [[6, 0, self.equipments]],
-						km: self.$('#equipmentKmSpent').val(),
+						km: self.$('#equipmentKmSpent').val().replace(',', '.'),
 						oil_qtity: self.$('#equipmentOilQtitySpent').val(),
-						oil_price: self.$('#equipmentOilPriceSpent').val(),
+						oil_price: self.$('#equipmentOilPriceSpent').val().replace(',', '.'),
 						state: app.Models.Task.state[1].value,
 						effective_hours: self.hours,
 						remaining_hours: 0,
@@ -788,6 +782,7 @@ app.Views.TasksListView = Backbone.View.extend({
 	    this.equipments = _.map($("#equipmentsDone").sortable('toArray'), function(equipment){ return _(_(equipment).strRightBack('_')).toNumber(); }); 
 	    this.equipments.push( input_equipment_id );
 
+
 		params = {
 		    //Project state
 		    project_state: newInterState,
@@ -799,8 +794,8 @@ app.Views.TasksListView = Backbone.View.extend({
 		    equipment_ids: this.equipments,
 		    vehicule: this.vehicule,
 	        km: this.$('#equipmentKmDone').val(),
-	        oil_qtity: this.$('#equipmentOilQtityDone').val(),
-	        oil_price: this.$('#equipmentOilPriceDone').val(),	
+	        oil_qtity: this.$('#equipmentOilQtityDone').val().replace(',', '.'),
+	        oil_price: this.$('#equipmentOilPriceDone').val().replace(',', '.'),
 	        
 		    //Task work params
              //name: task.name,
