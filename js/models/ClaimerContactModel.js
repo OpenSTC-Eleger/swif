8ddfa629921afc48012a7d8b46b83a4a7bda58c5
pyfadet <pierreyves.fadet@siclic.fr>
2013-05-06 16:18:21 +0200

    #651

--- js/models/ClaimerContactModel.js
+++ js/models/ClaimerContactModel.js
@@ -33,49 +33,55 @@ app.Models.ClaimerContact = Backbone.RelationalModel.extend({
         return response;
     },
     
-
-
-	save: function(id,data,closeModal, view, strRoute) { 
-		app.saveOE(id, data, this.model_name,app.models.user.getSessionID(), {
-            beforeSend: function(){
-				app.loader('display');
-        	},
-		    success: function (data) {
-		        console.log(data);
-		        if(data.error){
-		    		app.notify('', 'error', app.lang.errorMessages.unablePerformAction, app.lang.errorMessages.sufficientRights);
-		        }
-		        else{
-		        	if( closeModal!= null )
-		            	closeModal.modal('hide');
-		        	if( view || strRoute ) {
-		                if(app.collections.claimersContacts == null ){
-		                    app.collections.claimersContacts = new app.Collections.ClaimersContacts();
-		                }	
-		                //TODO fetch tasks & interventions pê pas necessaires car elles st rechargées dans le routeur
-					 	app.collections.claimersContacts.fetch({  
-					 		success: function(){
-						 		app.collections.claimers.fetch({
-					                success: function(){				 			
-						 				if( strRoute ) {
-											//route = Backbone.history.fragment;
-											Backbone.history.loadUrl(strRoute);
-						 					//app.Router.navigate("planning/"+Backbone.history.fragment,{trigger: true, replace: true})
-										}
-										else if (view)
-											view.render();
-							 		}					 
-						 		});
-					 		}					 
-					 	});
-					}
-		        }
-		    },
-		    error: function () {
-				console.log('ERROR - Unable to save the Request - RequestView.js');
-		    }, 
-		});
+    /** Save Officer
+	*/
+	save: function(data, id, options) { 
+		app.saveOE(id>0?id:0, data, this.model_name, app.models.user.getSessionID(),options);
 	},
+    
+
+
+//	save: function(id,data,closeModal, view, strRoute) { 
+//		app.saveOE(id, data, this.model_name,app.models.user.getSessionID(), {
+//            beforeSend: function(){
+//				app.loader('display');
+//        	},
+//		    success: function (data) {
+//		        console.log(data);
+//		        if(data.error){
+//		    		app.notify('', 'error', app.lang.errorMessages.unablePerformAction, app.lang.errorMessages.sufficientRights);
+//		        }
+//		        else{
+//		        	if( closeModal!= null )
+//		            	closeModal.modal('hide');
+//		        	if( view || strRoute ) {
+//		                if(app.collections.claimersContacts == null ){
+//		                    app.collections.claimersContacts = new app.Collections.ClaimersContacts();
+//		                }	
+//		                //TODO fetch tasks & interventions pê pas necessaires car elles st rechargées dans le routeur
+//					 	app.collections.claimersContacts.fetch({  
+//					 		success: function(){
+//						 		app.collections.claimers.fetch({
+//					                success: function(){				 			
+//						 				if( strRoute ) {
+//											//route = Backbone.history.fragment;
+//											Backbone.history.loadUrl(strRoute);
+//						 					//app.Router.navigate("planning/"+Backbone.history.fragment,{trigger: true, replace: true})
+//										}
+//										else if (view)
+//											view.render();
+//							 		}					 
+//						 		});
+//					 		}					 
+//					 	});
+//					}
+//		        }
+//		    },
+//		    error: function () {
+//				console.log('ERROR - Unable to save the Request - RequestView.js');
+//		    }, 
+//		});
+//	},
 
 
 
