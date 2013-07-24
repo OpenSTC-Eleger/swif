1a4c53679ec69a939b3aac41883d0d10e40409fe
Cedric Brancourt <cedric.brancourt@gmail.com>
2013-07-24 15:48:33 +0200

    Evolution #1209 Migrer la collection Claimers type vers l'API barakafrites

--- js/collections/ClaimersTypesCollection.js
+++ js/collections/ClaimersTypesCollection.js
@@ -1,44 +1,20 @@
 /******************************************
 * Claimers Types Collection
 */
+
+// fields = ["claimers", "code", "id", "name"]
+
 app.Collections.ClaimersTypes = app.Collections.GenericCollection.extend({
 
     model: app.Models.ClaimerType,
 
-    // Model name in the database //
-    model_name : 'openstc.partner.type',
-
-    url: 'type-demandeurs',
-
+    url: '/api/open_object/partner_types',
 
     /** Collection Initialization
     */
     initialize: function (options) {
     	//console.log('Claimer type collection Initialization');
     },
-
-
-
-    /** Collection Sync
-    */
-    sync: function(method, model, options) {
-        var fields = ["claimers", "code", "id", "name"];
-
-    	return app.readOE(this.model_name, app.models.user.getSessionID(), options, fields);
-    },
-
-
-
-	/** Collection Parse
-	*/
-	parse: function(response) { 
-		return response.result.records;
-//		return _.reject(response.result.records, function(record){
-//			return record.code == "ADMINISTRE";
-//		})
-	},
-
-
 
 	/** Comparator for ordering collection
     */
