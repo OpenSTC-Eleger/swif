f22f11c5976889165ee9ba933580006e1a37f57e
plancher bruno <bruno.plancher@gmail.com>
2013-08-06 14:12:57 +0200

    updated app.collections.equipments to use REST url

--- js/collections/EquipmentsCollection.js
+++ js/collections/EquipmentsCollection.js
@@ -8,7 +8,7 @@ app.Collections.Equipments = app.Collections.GenericCollection.extend({
 	// Model name in the database //
 	model_name : 'openstc.equipment',
 
-	url: 'equipments',
+	url: '/api/openstc/equipments',
 
 
 	/** Collection Initialization
@@ -19,28 +19,28 @@ app.Collections.Equipments = app.Collections.GenericCollection.extend({
 
 
 
-	/** Collection Sync
-	*/
-	sync: function(method, model, options) {
-		var fields = ["commercial_vehicle", "complete_name", "cv", "energy_type", "fat_material", "id", "immat", "km", "manager_id", "marque", "name", "service", "service_ids", "service_technical_id", "small", "small_material", "technical", "technical_vehicle", "time", "type", "type_prod", "usage", "year"];
-
-		return app.readOE(this.model_name, app.models.user.getSessionID(), options, fields);
-	},
-
-
-
-	/** Collection Parse
-	*/
-	parse: function(response) {
-		// keeps only STC materials //
-		var records = response.result.records;
-		records = _.filter(records, function(record){
-			return record.technical_vehicle || record.commercial_vehicle
-					|| record.fat_material || record.small_material;
-		});
-
-		return records;
-	},
+//	/** Collection Sync
+//	*/
+//	sync: function(method, model, options) {
+//		var fields = ["commercial_vehicle", "complete_name", "cv", "energy_type", "fat_material", "id", "immat", "km", "manager_id", "marque", "name", "service", "service_ids", "service_technical_id", "small", "small_material", "technical", "technical_vehicle", "time", "type", "type_prod", "usage", "year"];
+//
+//		return app.readOE(this.model_name, app.models.user.getSessionID(), options, fields);
+//	},
+//
+//
+//
+//	/** Collection Parse
+//	*/
+//	parse: function(response) {
+//		// keeps only STC materials //
+//		var records = response.result.records;
+//		records = _.filter(records, function(record){
+//			return record.technical_vehicle || record.commercial_vehicle
+//					|| record.fat_material || record.small_material;
+//		});
+//
+//		return records;
+//	},
 	
 	
 
