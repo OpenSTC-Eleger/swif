e45ccc6ad7a7461325bc203865af2fb7079b863f
pyfadet <pierreyves.fadet@siclic.fr>
2013-08-28 17:20:06 +0200

    ajout du champs total_hours dans le model et la collection des inters

--- js/models/InterventionModel.js
+++ js/models/InterventionModel.js
@@ -4,11 +4,9 @@
 app.Models.Intervention = Backbone.Model.extend({
 	
 	urlRoot: "/api/openstc/interventions",
-<<<<<<< HEAD
-	fields: ['id', 'name', 'description', 'tasks', 'state', 'service_id', 'site1', 'date_deadline', 'planned_hours', 'effective_hours', 'tooltip', 'progress_rate', 'overPourcent', 'actions','create_uid','ask_id'],
-=======
+
 	fieldsOE: ['id', 'name', 'description', 'tasks', 'state', 'service_id', 'site1', 'date_deadline', 'planned_hours', 'effective_hours', 'total_hours', 'tooltip', 'progress_rate', 'overPourcent', 'actions','create_uid','ask_id'],
->>>>>>> décomposition des vues et des templates pour le bandeau de gauche des inter dans le planning
+
 
 	searchable_fields: [
 		{
