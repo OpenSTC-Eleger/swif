21dba5a94ce0510997fe2ecfe60798bf310975ae
pyfadet <pierreyves.fadet@siclic.fr>
2013-08-29 09:22:54 +0200

    update TaskModel after merge whith refactoring branch

--- js/models/TaskModel.js
+++ js/models/TaskModel.js
@@ -241,8 +241,8 @@ app.Models.Task = Backbone.Model.extend({
 
 	/** Save Model
 	*/
-	save: function(id,data, options) {
-		app.saveOE(id, data, this.model_name,app.models.user.getSessionID(), options)
+//	save: function(id,data, options) {
+//		app.saveOE(id, data, this.model_name,app.models.user.getSessionID(), options)
 //		if( options==null ) {
 //			app.saveOE(id, data, this.model_name,app.models.user.getSessionID(), {         	
 //				success: function(data){
@@ -258,7 +258,7 @@ app.Models.Task = Backbone.Model.extend({
 //		else {
 //			app.saveOE(id, data, this.model_name,app.models.user.getSessionID(), options);
 //		}
-	},
+//	},
 	
 
 
