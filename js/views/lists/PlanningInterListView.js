db172b8f63419c0d17b9815905ab9a9b13b8ce8a
pyfadet <pierreyves.fadet@siclic.fr>
2013-09-05 12:40:20 +0200

    clean planning views

--- js/views/lists/PlanningInterListView.js
+++ js/views/lists/PlanningInterListView.js
@@ -24,8 +24,6 @@ app.Views.PlanningInterListView = Backbone.View.extend({
 			'switch-change .calendarSwitch'           : 'scheduledInter',
 			'switch-change #switchWithForeman'        : 'setForemanInTeam',
 		
-		
-			'change #interventionDetailService'       : 'fillDropdownService',	
 			'click #filterStateInterventionList li:not(.disabled) a' 	: 'setFilterState',	
 			'click a.modalCreateInter'			: 'displayModalSaveInter',
 			//'click #listAgents li a, #listTeams li a' :          'selectPlanning',	
@@ -182,117 +180,6 @@ app.Views.PlanningInterListView = Backbone.View.extend({
 			}
 
 			app.models.intervention.saveAndRoute(id, params, null, this);
-		},
-
-
-
-		/** Display the form to add a new Intervention
-		*/
-		displayFormAddIntervention: function(e){
-
-			//search no technical services
-			var noTechnicalServices = _.filter(app.collections.claimersServices.models, function(service){
-				return service.attributes.technical != true 
-			});
-			//remove no technical services
-			app.collections.claimersServices.remove(noTechnicalServices);
-
-			app.views.selectListServicesView = new app.Views.DropdownSelectListView({el: $("#interventionDetailService"), collection: app.collections.claimersServices})
-			app.views.selectListServicesView.clearAll();
-			app.views.selectListServicesView.addEmptyFirst();
-			app.views.selectListServicesView.addAll();
-			
-
-			// Fill select Places  //
-			app.views.selectListPlacesView = new app.Views.DropdownSelectListView({el: $("#interventionPlace"), collection: app.collections.places})
-			app.views.selectListPlacesView.clearAll();
-			app.views.selectListPlacesView.addEmptyFirst();
-			app.views.selectListPlacesView.addAll();	
-			
-			// Retrieve the ID of the intervention //
-			this.pos = e.currentTarget.id;
-			$('#modalAddInter').modal();
-	   },
-
-
-
-		renderService: function ( service ) {
-			if( service!= null ) {
-				app.views.selectListServicesView.setSelectedItem( service );
-				places = app.collections.places.models;
-				
-				//keep only places belongs to service selected
-				keepedPlaces = _.filter(places, function(item){
-					return $.inArray(service, item.toJSON().service_ids)!=-1
-				});
-				app.views.selectListPlacesView.collection = new app.Collections.Places(keepedPlaces);
-				app.views.selectListPlacesView.clearAll();
-				app.views.selectListPlacesView.addEmptyFirst();
-				app.views.selectListPlacesView.addAll();
-			}	
-		},
-
-
-
-		fillDropdownService: function(e){
-			e.preventDefault();
-			$('#interventionPlace').val('');
-			this.renderService( _($(e.target).prop('value')).toNumber() );
-		},
-
-
-
-
-
-		/** Save the intervention */
-		saveIntervention: function (e) {
-
-			e.preventDefault();
-
-			var self = this;
-			 
-			input_service_id = null;
-			if ( app.views.selectListServicesView && app.views.selectListServicesView.getSelected())
-				input_service_id = app.views.selectListServicesView.getSelected().toJSON().id;
-			 
-			var params = {	
-				name: this.$('#interventionName').val(),
-				description: this.$('#interventionDescription').val(),
-				state: this.$('#isTemplate').is(':checked')?"template":"open",
-				// active: this.$('#isTemplate').is(':checked')?false:true,
-				service_id: input_service_id,
-				site1: this.$('#interventionPlace').val(),
-				site_details: this.$('#interventionPlacePrecision').val(),
-			};
-
-			app.models.intervention.saveAndRoute(0,params,$('#modalAddInter'), this, "#planning");
-		},
-
-
-
-		saveNewState: function(params, element) {
-			var self = this;
-			self.element = element;
-			self.params = params
-			this.selectedInter.save(params, {
-				success: function (data) {
-						console.log(data);
-						if(data.error){
-							app.notify('', 'error', app.lang.errorMessages.unablePerformAction, app.lang.errorMessages.sufficientRights);
-						}
-						else{
-							console.log('NEW STATE INTER SAVED');
-							if( self.element!= null )
-								self.element.modal('hide');
-							self.selectedInter.update(self.params);
-							app.collections.interventions.add(self.selectedInter);
-							self.render();
-						}
-					},
-					error: function () {
-						console.log('ERROR - Unable to valid the Inter - InterventionView.js');
-					},
-			},false);
 		},
 
 
