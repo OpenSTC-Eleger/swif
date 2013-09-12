6015d0c8475803684a4d53c9172619871c0b1788
Cedric Brancourt <cedric.brancourt@gmail.com>
2013-09-12 16:57:32 +0200

    fixes #1398 Minify JS

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,30 +2,54 @@ module.exports = function (grunt) {
 	grunt.initConfig({
 		pkg   : grunt.file.readJSON('package.json'),
 		uglify: {
-			build: {
+			dist: {
 				files: {
-					'js/swif.min.js': ['js/**/*.js','!js/lib/**', '!js/**.min.js']
+					//'js/swif.min.js': ['js/**/*.js','!js/libs/**', '!js/**.min.js'],
+					'dist/js/collections.min.js': ['js/collections/**/*.js'],
+					'dist/js/helpers.min.js': ['js/helpers/**/*.js'],
+					'dist/js/models.min.js': ['js/models/**/*.js'],
+					'dist/js/routers.min.js': ['js/routers/**/*.js'],
+					'dist/js/views.min.js': ['js/views/**/*.js'],
+					'dist/js/app.min.js': ['js/app.js'],
+					'dist/js/generics.min.js': [
+						'js/models/GenericModel.js',
+						'js/collections/GenericCollection.js',
+						'js/views/lists/GenericListView.js',
+						'js/views/modals/GenericModalView.js'
+					]
 				}
 			},
 			options: {
-				compress: true
+				compress: true,
+				mangle: false
 			}
 		},
+		// LESS compilation options
 		less: {
-			options: {
-				compress: true,
-				yuicompress: true
-			},
-			build: {
+
+			dist: {
 				files: {
 					"css/swif.css": "css/startup.less"
+				},
+				options: {
+					compress: true,
+					yuicompress: true
 				}
 			}
 
+		},
+		targethtml: {
+			dist: {
+				files : {
+					'dist/index.html': 'index.html'
+				}
+			}
 		}
 
 	});
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-targethtml');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 
 };
