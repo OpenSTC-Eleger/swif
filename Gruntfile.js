da91b443d66af563cec325362461a7e0cd2debb1
Cedric Brancourt <cedric.brancourt@gmail.com>
2013-09-13 16:48:42 +0200

    fixes #1403 Package Application It builds all with grunt command ... That's all ;)

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,16 @@
 module.exports = function (grunt) {
 	grunt.initConfig({
-		pkg   : grunt.file.readJSON('package.json'),
-		uglify: {
-			dist: {
+		pkg       : grunt.file.readJSON('package.json'),
+		uglify    : {
+			dist   : {
 				files: {
-					//'js/swif.min.js': ['js/**/*.js','!js/libs/**', '!js/**.min.js'],
 					'dist/js/collections.min.js': ['js/collections/**/*.js'],
-					'dist/js/helpers.min.js': ['js/helpers/**/*.js'],
-					'dist/js/models.min.js': ['js/models/**/*.js'],
-					'dist/js/routers.min.js': ['js/routers/**/*.js'],
-					'dist/js/views.min.js': ['js/views/**/*.js'],
-					'dist/js/app.min.js': ['js/app.js'],
-					'dist/js/generics.min.js': [
+					'dist/js/helpers.min.js'    : ['js/helpers/**/*.js'],
+					'dist/js/models.min.js'     : ['js/models/**/*.js'],
+					'dist/js/routers.min.js'    : ['js/routers/**/*.js'],
+					'dist/js/views.min.js'      : ['js/views/**/*.js'],
+					'dist/js/app.min.js'        : ['js/app.js'],
+					'dist/js/generics.min.js'   : [
 						'js/models/GenericModel.js',
 						'js/collections/GenericCollection.js',
 						'js/views/lists/GenericListView.js',
@@ -21,18 +20,18 @@ module.exports = function (grunt) {
 			},
 			options: {
 				compress: true,
-				mangle: false
+				mangle  : false
 			}
 		},
 		// LESS compilation options
-		less: {
+		less      : {
 
 			dist: {
-				files: {
-					"css/swif.css": "css/startup.less"
+				files  : {
+					"dist/css/swif.css": "css/startup.less"
 				},
 				options: {
-					compress: true,
+					compress   : true,
 					yuicompress: true
 				}
 			}
@@ -40,11 +39,19 @@ module.exports = function (grunt) {
 		},
 		targethtml: {
 			dist: {
-				files : {
+				files: {
 					'dist/index.html': 'index.html'
 				}
 			}
+		},
+		copy      : {
+			dist: {
+				files: {
+					'dist/': ['js/libs/**.js','i18n/**', 'templates/**','config/*', 'properties.json', 'font/*']
+				}
+			}
 		}
+
 
 	});
 	grunt.loadNpmTasks('grunt-contrib-uglify');
@@ -52,4 +59,5 @@ module.exports = function (grunt) {
 	grunt.loadNpmTasks('grunt-targethtml');
 	grunt.loadNpmTasks('grunt-contrib-copy');
 
+	grunt.registerTask('default', ['less','uglify','targethtml','copy']);
 };
---
+++ README.md
@@ -0,0 +1,33 @@
+OpenSTC By Siclic http://www.siclic.fr
+
+Install Grunt
+=============
+
+    $: npm install -g grunt-cli
+
+Build Swif
+==========
+
+Build all in dist/ :
+
+    $: grunt
+
+Build individuals
+=================
+
+Compile less stylesheets
+
+    $: grunt less
+
+Minify and Uglify JS
+
+    $: grunt uglify
+
+Copy Files
+
+    $: grunt copy
+
+Build HTML index
+
+    $: grunt targethtml
+
