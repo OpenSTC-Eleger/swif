5e5127758418ace77a43733fa949d4d8e2d71fcc
plancher bruno <bruno.plancher@gmail.com>
2013-09-16 16:42:56 +0200

    updated 'print' feature of intervention list to adapt info according to has_equipment flag

--- js/views/items/ItemInterventionView.js
+++ js/views/items/ItemInterventionView.js
@@ -126,7 +126,27 @@ app.Views.ItemInterventionView = Backbone.View.extend({
 			if(interJSON.date_deadline != false){
 				$('#interDeadline').html(' / ' + moment(interJSON.date_deadline).format('LL'));
 			}
-			$('#interPlace').html(interJSON.site1[1]);
+			if(interJSON.has_equipment){
+				//display location (openstc.site) info
+				$('#printPlaceLabel').css({display:'inline-block'});
+				$('#interPlace').css({display:'inline-block'});
+				
+				//fill data with equipment and location
+				$('#interPlaceOrEquipment').html(interJSON.equipment_id[1]);
+				$('#interPlace').html(interJSON.site1[1]);
+				$('#printPlaceOrEquipmentLabel').html(app.lang.equipment + ':');
+				$('#printPlaceLabel').html(app.lang.location + ':');
+				
+			}
+			else{
+				//hide location info (keeping only site info on placeOrEquipment field)
+				$('#printPlaceLabel').css({display:'none'});
+				$('#interPlace').css({display:'none'});
+				
+				//fill data of site1
+				$('#interPlaceOrEquipment').html(interJSON.site1[1]);
+				$('#printPlaceOrEquipmentLabel').html(app.lang.place + ':');
+			}
 			$('#interPlaceMore').html(interJSON.site_details);
 
 			$('#printTask').printElement({
