8aaa6afbc421d8c1489cff1b1a5bf21db3198b98
Simon <simon.mahe@siclic.fr>
2013-09-17 14:45:11 +0200

    Refacto Modal pour déplanifier une tâche

--- js/views/modals/ModalUnplanTaskView.js
+++ js/views/modals/ModalUnplanTaskView.js
@@ -6,15 +6,12 @@ app.Views.ModalUnplanTaskView =  app.Views.GenericModalView.extend({
 
 	templateHTML: 'modals/modalUnplanTask',
 
-	modal : null,
-
-	createMode : false,
 
 	
 	// The DOM events //
 	events: function(){
 		return _.defaults({
-			'click #btnRemoveTask'      : 'removeTaskFromSchedule'
+			'click #btnRemoveTask'     : 'removeTaskFromSchedule'
 		}, 
 			app.Views.GenericModalView.prototype.events
 		);
@@ -25,12 +22,13 @@ app.Views.ModalUnplanTaskView =  app.Views.GenericModalView.extend({
 	*/
 	initialize : function() {
 		var self = this;
-		this.calendarModel = this.options.calendarModel;
-		this.panelModel = this.options.panelModel;
+
 		this.modal = $(this.el);
+
+		// Intervention Model in the Left column //
+		this.panelModel = this.options.panelModel;
+
 		this.render();
-
-
 	},
 
 
@@ -45,57 +43,12 @@ app.Views.ModalUnplanTaskView =  app.Views.GenericModalView.extend({
 		$.get("templates/" + this.templateHTML + ".html", function(templateData){
 		 
 
-			var template = _.template(templateData, {lang: app.lang});	
+			var template = _.template(templateData, {
+				lang : app.lang,
+				task : self.model
+			});
 			
-
 			self.modal.html(template);
-			
-			
-	        // Reset the modal Buttons //
-	        $('#btnRemoveTask').prop('disabled', false);
-	       	$('#switchWithForeman').bootstrapSwitch('setActive', true);
-	        
-			
-	       
-	
-			// Set informations in the modal //
-			var taskInter = '';
-			if(self.calendarModel.getInterventionId() != ''){ taskInter = "<i class='icon-pushpin'></i> " + self.calendarModel.getInterventionName() + " -"; }
-			var tasksInfo = taskInter + " " + self.calendarModel.getName();
-	
-			// Display a label with the state of the task //
-			
-			tasksInfo += '<span class="label label-'+app.Models.Task.status[self.calendarModel.getState()].color+' pull-right">'+app.Models.Task.status[self.calendarModel.getState()].translation+'</span>';
-	
-	
-			// Check if the task is set to an officer or a team //
-			if(self.calendarModel.getTeamId() == false){ 
-				var assignTo = "<br /> <i class='icon-user'></i> " + self.calendarModel.getUserName();
-				$('#formModalAboutTask').hide();
-			}
-			else{
-				var assignTo = "<br /><i class='icon-group'></i> " + self.calendarModel.getTeamName();
-				$('#formModalAboutTask').show();
-			}
-	
-			$('#infoModalAboutTask p').html(tasksInfo);
-			$('#infoModalAboutTask small').html(self.calendarModel.getStartEndDateInformations() + assignTo);
-	
-			// Disable or not the button "Remove Of The Schedule" //
-			if(self.calendarModel.getState() == app.Models.Task.status.done.key || self.calendarModel.getState() == app.Models.Task.status.cancelled.key){
-	        	$('#btnRemoveTask').prop('disabled', true);
-	        	$('#switchWithForeman').bootstrapSwitch('setActive', false);
-			}
-	
-	
-			// Set the ID of the Task in the DOM of the modal //
-			$('#modalAboutTask').data('taskId', self.calendarModel.getId());
-	
-	
-			// Display the Modal //
-			//$("#modalAboutTask").modal('show');
-
-
 			self.modal.modal('show');
 		});
 
@@ -109,29 +62,30 @@ app.Views.ModalUnplanTaskView =  app.Views.GenericModalView.extend({
 	removeTaskFromSchedule: function(e){
 
 		var self = this;
+
+		// Set the button in loading State //
+		$(e.target).button('loading');
 		
 		params = {
-			state: app.Models.Task.status.draft.key,
-			user_id: false,
-			team_id: false,
-			date_end: false,
+			state     : app.Models.Task.status.draft.key,
+			user_id   : false,
+			team_id   : false,
+			date_end  : false,
 			date_start: false,
 		};
 
-		this.calendarModel.save(params, {patch: true, silent: false})
+		this.model.save(params, {patch: true, silent: false})
 			.done(function(data) {
 				self.modal.modal('hide');
 				if(!_.isUndefined(self.panelModel))
 					self.panelModel.fetch({ data : {fields : self.panelModel.fields} });
-				self.calendarModel.fetch({ data : {fields : self.calendarModel.fields} });
+				self.model.fetch({ data : {fields : self.model.fields} });
 			})
-			.fail(function (e) {
-				console.log(e);
+			.always(function(){
+				// Reset the button state //
+				$(e.target).button('reset');
 			})
-		
-	},
-
-
+	}
 
 
 });
