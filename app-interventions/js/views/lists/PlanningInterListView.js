7e3a4affea2f1576314d7e048b37b0397d154749
pyfadet <pierreyves.fadet@siclic.fr>
2013-11-19 13:04:10 +0100

    refs #1874 : requirification planning first

--- app-interventions/js/views/lists/PlanningInterListView.js
+++ app-interventions/js/views/lists/PlanningInterListView.js
@@ -1,202 +1,224 @@
-/******************************************
-* Pagination View
-*/
-app.Views.PlanningInterListView = app.Views.GenericListView.extend({
-
-	el           : '#planningInters',
+define([
+	'app',
 	
-	templateHTML : 'planningInters',
+	'taskModel',
+	'interventionModel',
 
-	currentRoute : null,
+	'genericListView',
+	'paginationView',
+	'itemPlanningInterView',
+	'itemPlanningInterTaskListView',
+	'modalInterventionView'
 
-	urlParameters : ['id', 'officer', 'team', 'year', 'week', 'filter', 'sort', 'page', 'search'],
-	
-	// The DOM events //
-	events: function(){
-		return _.defaults({			
-			//'switch-change #switchWithForeman'        : 'setForemanInTeam',	
-			'click #filterStateInterventionList li a' 	: 'setFilterState',	
-			'click a.modalCreateInter'			: 'displayModalSaveInter',
-			'click #pagination ul a'			: 'goToPage',
-		}, 
-			app.Views.GenericListView.prototype.events
-		);
-	},
+], function(app, TaskModel, InterventionModel, GenericListView, PaginationView , ItemPlanningInterView , ItemPlanningInterTaskListView, ModalInterventionView ){
+
+	'use strict';
 
 
-	/** View Initialization
+	/******************************************
+	* Pagination View
 	*/
-	initialize: function(params) {
-		this.options = params;
-
-		var self = this;
-		
-		this.filterValue = 'state-open';
-		
-		this.collections = params.collections;
-		app.router.render(this);
-	    
-	    this.collections.interventions.off();
-	    this.listenTo(self.collections.interventions, 'add', this.addInter);
-	},
+	var planningInterListView = GenericListView.extend({
 	
-	addInter: function(model){
-		//If filter is on 'all state' or 'scheduled' add item view in panel
-		if( _.isUndefined(this.filterValue) || _(this.filterValue).strRight('-') == model.toJSON().state ) {
-			var itemPlanningInterTaskListView = new app.Views.ItemPlanningInterTaskListView({ model : model });	
-			var itemPlanningInterView  = new app.Views.ItemPlanningInterView({ model: model, detailedView:itemPlanningInterTaskListView });
-			$('#inter-items').prepend(itemPlanningInterTaskListView.render().el);
-			$('#inter-items').prepend(itemPlanningInterView.render().el);
-			itemPlanningInterView.highlight().done(function(){
-				itemPlanningInterView.expendAccordion();
-			});
-			if(this.collections.interventions.length == 1){
-				$(this.el).find('.noInter').css({display: 'none'});
-				$(this.el).find('.table-nested-objects').css({display: 'table'});
-			}			
-		}
-		else {
-			//filter is selected and intervention has been created : display open interventions
-			$( "#filterStateInterventionList li" ).children("a[href='#'" + app.Models.Task.status.open.key + "]").trigger( "click" );
-		}
-		app.notify('', 'success', app.lang.infoMessages.information, model.getName()+' : '+app.lang.infoMessages.interventionSaveOK);
-	},
-
-	/** Display the view
-	*/
-	render: function() {
-		var self = this;		
+		el           : '#planningInters',
+		
+		templateHTML : 'lists/planningInters',
 	
-		$.get("templates/" + this.templateHTML + ".html", function(templateData){
-			// Set variables template //
-			var template = _.template(templateData, {
-				lang: app.lang,
-				interventionsState: app.Models.Intervention.status,
-				interventions: self.collections.interventions.toJSON(),		
+		currentRoute : null,
+	
+		urlParameters : ['id', 'officer', 'team', 'year', 'week', 'filter', 'sort', 'page', 'search'],
+		
+		// The DOM events //
+		events: function(){
+			return _.defaults({			
+				//'switch-change #switchWithForeman'        : 'setForemanInTeam',	
+				'click #filterStateInterventionList li a' 	: 'setFilterState',	
+				'click a.modalCreateInter'			: 'displayModalSaveInter',
+				'click #pagination ul a'			: 'goToPage',
+			}, 
+				GenericListView.prototype.events
+			);
+		},
+	
+	
+		/** View Initialization
+		*/
+		initialize: function(params) {
+			this.options = params;
+	
+			var self = this;
+			
+			this.filterValue = 'state-open';
+			
+			this.collections = params.collections;
+			app.router.render(this);
+		    
+		    this.collections.interventions.off();
+		    this.listenTo(self.collections.interventions, 'add', this.addInter);
+		},
+		
+		addInter: function(model){
+			//If filter is on 'all state' or 'scheduled' add item view in panel
+			if( _.isUndefined(this.filterValue) || _(this.filterValue).strRight('-') == model.toJSON().state ) {
+				var itemPlanningInterTaskListView = new ItemPlanningInterTaskListView({ model : model });	
+				var itemPlanningInterView  = new ItemPlanningInterView({ model: model, detailedView:itemPlanningInterTaskListView });
+				$('#inter-items').prepend(itemPlanningInterTaskListView.render().el);
+				$('#inter-items').prepend(itemPlanningInterView.render().el);
+				itemPlanningInterView.highlight().done(function(){
+					itemPlanningInterView.expendAccordion();
+				});
+				if(this.collections.interventions.length == 1){
+					$(this.el).find('.noInter').css({display: 'none'});
+					$(this.el).find('.table-nested-objects').css({display: 'table'});
+				}			
+			}
+			else {
+				//filter is selected and intervention has been created : display open interventions
+				$( "#filterStateInterventionList li" ).children("a[href='#'" + TaskModel.status.open.key + "]").trigger( "click" );
+			}
+			app.notify('', 'success', app.lang.infoMessages.information, model.getName()+' : '+app.lang.infoMessages.interventionSaveOK);
+		},
+	
+		/** Display the view
+		*/
+		render: function() {
+			var self = this;		
+		
+			$.get(app.moduleUrl+"/templates/" + this.templateHTML + ".html", function(templateData){
+				// Set variables template //
+				var template = _.template(templateData, {
+					lang: app.lang,
+					interventionsState: InterventionModel.status,
+					interventions: self.collections.interventions.toJSON(),		
+				});
+				
+				$(self.el).html(template);
+				
+				// Call the render Generic View //
+				GenericListView.prototype.render(self.options);
+				
+				$('*[rel="tooltip"]').tooltip();
+				$('*[rel="popover"]').popover({trigger: 'hover', delay: { show: 500, hide: 100 }});
+	
+				
+				$('.timepicker-default').timepicker({ showMeridian: false, disableFocus: true, showInputs: false, modalBackdrop: false});
+	
+	
+				// Set the focus to the first input of the form //
+				$('#modalAddInter, #modalAddTask').on('shown', function (e) {
+					$(this).find('input').first().focus();
+				})
+							
+				_.each(self.collections.interventions.models, function(inter, i){
+					
+					var detailedView =new ItemPlanningInterTaskListView({model: inter});
+					var simpleView = new ItemPlanningInterView({model: inter, detailedView:detailedView});
+					$('#inter-items').append( simpleView.render().el );
+					$('#inter-items').append(detailedView.render().el);
+					simpleView.detailedView = detailedView;				
+				});	
+				
+				
+				
+				// Pagination view //
+				app.views.paginationView = new PaginationView({ 
+					page       : self.options.page.page,
+					collection : self.collections.interventions,
+					size		: 'sm',
+					displayGoToPage : false,
+					
+				})
+				app.views.paginationView.render();
+				
+				
+				// Render Filter Link //
+				if(_.isUndefined(self.options.filter)){
+					
+					$('#filterStateIntervention').addClass('filter-disabled');
+					$('#filterStateInterventionList li.delete-filter').addClass('disabled');
+				}
+				else{
+					// set status information on filter selected
+					$('#filterStateIntervention').removeClass('filter-disabled');
+					$('#filterStateInterventionList li.delete-filter').removeClass('disabled');
+					if( !_.isUndefined( InterventionModel.status[self.options.filter.value] ) ) 
+						$('a.filter-button').addClass('text-' + InterventionModel.status[self.options.filter.value].color);
+				}
+				
 			});
-			
-			$(self.el).html(template);
-			
-			// Call the render Generic View //
-			app.Views.GenericListView.prototype.render(self.options);
-			
-			$('*[rel="tooltip"]').tooltip();
-			$('*[rel="popover"]').popover({trigger: 'hover', delay: { show: 500, hide: 100 }});
-
-			
-			$('.timepicker-default').timepicker({ showMeridian: false, disableFocus: true, showInputs: false, modalBackdrop: false});
-
-
-			// Set the focus to the first input of the form //
-			$('#modalAddInter, #modalAddTask').on('shown', function (e) {
-				$(this).find('input').first().focus();
-			})
-						
-			_.each(self.collections.interventions.models, function(inter, i){
-				
-				var detailedView =new app.Views.ItemPlanningInterTaskListView({model: inter});
-				var simpleView = new app.Views.ItemPlanningInterView({model: inter, detailedView:detailedView});
-				$('#inter-items').append( simpleView.render().el );
-				$('#inter-items').append(detailedView.render().el);
-				simpleView.detailedView = detailedView;				
-			});	
-			
-			
-			
-			// Pagination view //
-			app.views.paginationView = new app.Views.PaginationView({ 
-				page       : self.options.page.page,
-				collection : self.collections.interventions,
-				size		: 'sm',
-				displayGoToPage : false,
-				
-			})
+			return this;
+		},
+		
+		/**
+		 * Re calculate pagination 
+		 */
+		paginationRender : function(params) {
+			app.views.paginationView.initialize(params.page);
 			app.views.paginationView.render();
+		},
+		
+	
+		/** Display the form to add / update an intervention
+		*/
+		displayModalSaveInter: function(e){
+			e.preventDefault();
+			var params = {el:'#modalSaveInter',interventions: this.collections.interventions}
+			new app.Views.ModalInterventionView(params);
+		},
+	
+	
+		/** Filter Intervention by status
+		*/
+		setFilterState: function(e){
+	
+			e.preventDefault();
+			if($(e.target).parent().hasClass('disabled')) return;
 			
 			
-			// Render Filter Link //
-			if(_.isUndefined(self.options.filter)){
-				
-				$('#filterStateIntervention').addClass('filter-disabled');
-				$('#filterStateInterventionList li.delete-filter').addClass('disabled');
+			delete this.options.page;
+			delete this.options.sort;
+			delete this.options.filter;
+	
+			var filterValue = _($(e.target).attr('href')).strRightBack('#');
+			
+			// Set the filter value in the options of the view //
+			var globalSearch = {};
+			if(filterValue != 'delete-filter'){
+				this.options.filter =  'state-' + filterValue;
 			}
 			else{
-				// set status information on filter selected
-				$('#filterStateIntervention').removeClass('filter-disabled');
-				$('#filterStateInterventionList li.delete-filter').removeClass('disabled');
-				if( !_.isUndefined( app.Models.Intervention.status[self.options.filter.value] ) ) 
-					$('a.filter-button').addClass('text-'+app.Models.Intervention.status[self.options.filter.value].color);
+				delete this.options.filter;
 			}
 			
-		});
-		return this;
-	},
-	
-	/**
-	 * Re calculate pagination 
-	 */
-	paginationRender : function(params) {
-		app.views.paginationView.initialize(params.page);
-		app.views.paginationView.render();
-	},
-	
-
-	/** Display the form to add / update an intervention
-	*/
-	displayModalSaveInter: function(e){
-		e.preventDefault();
-		var params = {el:'#modalSaveInter',interventions: this.collections.interventions}
-		new app.Views.ModalInterventionView(params);
-	},
-
-
-	/** Filter Intervention by status
-	*/
-	setFilterState: function(e){
-
-		e.preventDefault();
-		if($(e.target).parent().hasClass('disabled')) return;
-		
-		
-		delete this.options.page;
-		delete this.options.sort;
-		delete this.options.filter;
-
-		var filterValue = _($(e.target).attr('href')).strRightBack('#');
-		
-		// Set the filter value in the options of the view //
-		var globalSearch = {};
-		if(filterValue != 'delete-filter'){
-			this.options.filter =  'state-' + filterValue;
-		}
-		else{
-			delete this.options.filter;
-		}
-		
-		this.filterValue = this.options.filter;
-		
-		app.views.planning.partialRender();
-		app.views.paginationView.render();
-
-	},	
-	
-	/**
-	 * Go to page
-	 */
-	goToPage: function(e){
-		e.preventDefault()
-		
-		delete this.options.page;
-		delete this.options.sort;
-		delete this.options.filter;
-
-		this.options.filter =   this.filterValue;		
-				
-		var link = $(e.target);
-		
-		this.options.page = _(link.attr('href')).strRightBack('/page');		
+			this.filterValue = this.options.filter;
 			
-		app.views.planning.partialRender();		
-	},
+			app.views.planning.partialRender();
+			app.views.paginationView.render();
+	
+		},	
+		
+		/**
+		 * Go to page
+		 */
+		goToPage: function(e){
+			e.preventDefault()
+			
+			delete this.options.page;
+			delete this.options.sort;
+			delete this.options.filter;
+	
+			this.options.filter =   this.filterValue;		
+					
+			var link = $(e.target);
+			
+			this.options.page = _(link.attr('href')).strRightBack('/page');		
+				
+			app.views.planning.partialRender();		
+		},
+	
+	});
+	
+		
+	return planningInterListView;
 
 });
---
+++ app-interventions/js/views/others/PlanningView.js
@@ -0,0 +1,197 @@
+define([
+	'app',
+	'appHelpers',
+
+	'officersCollection',
+	'teamsCollection',
+	'interventionsCollection',
+	'tasksCollection',
+	
+	'interventionModel',
+	'userModel',
+
+	'genericListView',
+	'paginationView',
+	'planningView',
+	'calendarView',
+	'planningInterListView'
+
+], function(app, AppHelpers,
+				OfficersCollection, TeamsCollection, InterventionsCollection, TasksCollection, 
+				InterventionModel, UserModel,
+				GenericListView, PaginationView , PlanningView , CalendarView, PlanningInterListView ){
+
+	'use strict';
+
+	/******************************************
+	* Planning View
+	*/
+	var planningView = Backbone.View.extend({
+	
+	
+		el : '#rowContainer',
+	
+		templateHTML: 'others/planning',
+	
+		filters: 'interventionsListFilter',
+	
+		
+		
+		selectedInter : '',
+		selectedTask  : '',
+	
+		sstoragePlanningSelected: 'selectedPlanning',
+		
+		// The DOM events //
+		events: {		
+	
+		},
+	
+	
+	
+		/** View Initialization
+		*/
+		initialize : function(params) {
+			var self = this;
+	
+			this.options = params;
+	
+			//By default display open intervention (intervention to schedule)
+			this.options.filter = 'state-open';
+			
+			
+			
+		    console.log("Planning Details view intialization")
+		    this.initCollections().done(function(){	    	
+		    	app.router.render(self);		
+		    });
+		},
+	
+	
+	
+		/** Display the view
+		*/
+		render : function() {
+			var self = this;
+	
+	
+			// Change the page title //
+			app.router.setPageTitle(app.lang.viewsTitles.planning);
+	
+	
+			// Retrieve the Login template // 
+			$.get(app.moduleUrl+"/templates/" + this.templateHTML + ".html", function(templateData){
+	
+	
+				// Change the page title //
+				app.router.setPageTitle(app.lang.viewsTitles.planning);
+	
+				
+				var options = self.options
+				self.collections.officers = UserModel.getOfficers();
+				self.collections.teams = UserModel.getTeams();
+				options.collections = self.collections;
+	
+				// Set variables template //
+				var template = _.template(templateData, {
+					lang: app.lang,			
+				});
+	
+				$(self.el).html(template);
+				//calendar panel
+				$('#calendar').append( new CalendarView(self.options).render().el );	
+				//interventions left panel			
+				app.views.planningInterListView = new PlanningInterListView(self.options)
+	
+	
+			});
+			$(this.el).hide().fadeIn('slow');
+			return this;
+		},
+	
+	
+	
+		/** Partial Render of the view
+		*/
+		partialRender: function () {
+			var self = this;
+			this.initCollections().done(function(){
+				app.views.planningInterListView.render();
+			});
+		},
+	
+		/**
+		 * Init intervention and task collections
+		 */
+		initCollections: function(){
+			var self = this;
+			
+			// Check if the collections is instantiate //
+			if(_.isUndefined(this.collections)){this.collections = {}}
+			if(_.isUndefined(this.collections.interventions)){this.collections.interventions = new InterventionsCollection();}
+			if(_.isUndefined(this.collections.tasks)){ this.collections.tasks = new TasksCollection(); }
+	
+	
+			// Check the parameters //
+			if(_.isUndefined(this.options.sort)){
+				this.options.sort = this.collections.interventions.default_sort;
+			}
+			else{
+				this.options.sort = AppHelpers.calculPageSort(this.options.sort);	
+			}
+			this.options.page = AppHelpers.calculPageOffset(this.options.page);
+	
+			if(!_.isUndefined(this.options.filter)){
+				this.options.filter = AppHelpers.calculPageFilter(this.options.filter);
+			}
+	
+			// Create Fetch params //
+			this.fetchParams = {
+				silent : true,
+				data   : {
+					limit  : app.config.itemsPerPage,
+					offset : this.options.page.offset,
+					sort   : this.options.sort.by+' '+this.options.sort.order
+				}
+			};
+	
+			var globalSearch = {};
+			if(!_.isUndefined(this.options.search)){
+				globalSearch.search = this.options.search;
+			}
+			if( !_.isUndefined(this.options.filter) ){
+				globalSearch.filter = this.options.filter;
+			}
+	
+			if(!_.isEmpty(globalSearch)){
+				this.fetchParams.data.filters = AppHelpers.calculSearch(globalSearch, InterventionModel.prototype.searchable_fields);
+			}
+			
+			return this.fetchCollections();
+		
+		},
+	
+	
+	
+		/**
+		* Fetch collections
+		*/
+		fetchCollections : function(){
+			var self = this;
+			//Fetch officers, teams, and interventions collections
+			return $.when(
+				UserModel.queryManagableOfficers(), 
+				UserModel.queryManagableTeams(),
+				this.collections.interventions.fetch(this.fetchParams)
+			)
+			.fail(function(e){
+				console.error(e);
+			})
+		}
+	
+	});
+	
+			
+	return planningView;
+
+});
