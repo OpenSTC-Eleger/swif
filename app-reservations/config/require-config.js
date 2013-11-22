53c86671aa00613d1cddd1629420eddcc24c6cc2
plancher bruno <bruno.plancher@gmail.com>
2013-11-22 09:13:15 +0100

    first step of RequiredJSIfied of FormBookingViews and Bookables

--- app-reservations/config/require-config.js
+++ app-reservations/config/require-config.js
@@ -13,15 +13,17 @@ requirejs.config({
 		bookingsCollection        			: 'app-reservations/js/collections/BookingsCollection',
 		bookingLinesCollection          	: 'app-reservations/js/collections/BookingLinesCollection',
 		bookingRecurrencesCollection        : 'app-reservations/js/collections/BookingRecurencesCollection',
-		
+		bookablesCollection	             	: 'app-reservations/js/collections/BookablesCollection',
 
 		// Models //
 		bookingModel                		: 'app-reservations/js/models/BookingModel',
 		bookingLineModel                	: 'app-reservations/js/models/BookingLineModel',
 		bookingRecurrenceModel             	: 'app-reservations/js/models/BookingRecurrenceModel',
+		bookableModel		             	: 'app-reservations/js/models/BookableModel',
 
 		// Views //
 		bookingsListView            		: 'app-reservations/js/views/lists/BookingsListView',
+		formBookingView						: 'app-reservations/js/views/forms/FormBookingView',
 		
 		// Items //
 		itemBookingView            			: 'app-reservations/js/views/items/ItemBookingView',
