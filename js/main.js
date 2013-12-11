f0b9bbe22193bf037934c4ccd69f35db8eed32eb
Cedric Brancourt <cedric.brancourt@gmail.com>
2013-12-11 14:30:31 +0100

    fixes #2051 Add download link on booking list for invoices

--- js/main.js
+++ js/main.js
@@ -42,6 +42,7 @@ define('main', [
 				app.collections.users  = new UsersCollection();
 
 
+                // DO NOT USE THIS (it is a nonsense, ask Cedric for details, when reading value use the app.current_user interface)
 				app.models.user = new UserModel();
 				if(_.isNull(localStorage.getItem('users-collection'))){
 					app.collections.users.add(app.models.user);
@@ -53,6 +54,8 @@ define('main', [
 					app.models.user.set(detailsUser);
 				}
 
+                // Interface to current_user, this should not be a collection stored in localstorage but just some current_user client state.
+                app.current_user = app.models.user
 
 				// UNABLE TO FETCH WITH BACKBONE LOCALSTORAGE AND REQUIRE JS //
 				/*app.collections.users.fetch();
