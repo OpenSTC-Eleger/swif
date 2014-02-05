9b4e1522f11a442186b5011a55d9df80355ec6e2
pyfadet <pierreyves.fadet@siclic.fr>
2014-02-05 15:22:04 +0100

    refs #2415 : href for filters --> extract code from template to js file and refacto

--- js/views/others/RecordFilterView.js
+++ js/views/others/RecordFilterView.js
@@ -16,6 +16,12 @@ define([
 		
 		components : [],
 		
+		filters	   : [],
+		
+		//Static variables uses to build url
+		URL_CTXT    : '/filter/',
+		SLASH_CHAR	: '/',
+		
 		// The DOM events //
 		events: {
 		},
@@ -24,26 +30,13 @@ define([
 		/** View Initialization
 		*/
 		initialize: function(options){	
-			var self = this;
+			
 			//init view parameters
 			this.el = options.el;
 			this.metaDataModel = options.metaDataModel;
 			this.states = options.states;
-			//init current route	
-			this.currentRoute = Backbone.history.fragment
-			this.urlArray = [this.currentRoute];			
-			if( _.str.include(this.currentRoute, "/filter/") ) 
-			{
-				this.urlArray = _.words(this.currentRoute, "/filter/");
-				var filter = parseInt(this.urlArray[1][0]);
-				if( _.isNaN(filter) ) 
-					this.urlArray[1] = '/' + _(this.urlArray[1]).strRight('/');
-				else
-					if( _.str.include(this.urlArray[1],'/') )
-						this.urlArray[1] = '/' + _(this.urlArray[1]).strRight('/');
-					else
-						this.urlArray[1] = "";
-			}
+			//prepares variables to construct url filter in template	
+			this.prepareUrls();
 
 			//Fetch meta model
 			this.metaDataModel.fetch();
@@ -57,15 +50,17 @@ define([
 			var self = this;
 			
 			//After get filters render view
-			this.initFilters().done(function(){
+			this.initFilters().done(function(){				
+
 				// Retrieve the template //
 				$.get(self.templateHTML, function(templateData){
+					
+					self.buildUrls();
 				
 					var template = _.template(templateData, {
 						lang   		: app.lang,
-						urlArray  	: self.urlArray, 
 						states 		: self.states,
-						filters		: self.metaDataModel.getFilters()
+						filters		: self.filters
 					});
 				
 					$(self.el).html(template);
@@ -79,10 +74,56 @@ define([
 		},
 		
 
-		//Get recording filters for model
+		/**
+		 * Get recording filters for model
+		 */
 		initFilters: function(){
 			var self = this;
 			return this.metaDataModel.filters()
+		},
+		
+		/**
+		 * Init Urls varaiables using to buils url filters in templates
+		 */
+		prepareUrls: function(){
+			this.urlLeftPart = Backbone.history.fragment;
+			this.urlRightPart = null;
+			if( _.str.include(this.urlLeftPart, this.URL_CTXT) ) 
+			{
+				this.urlArray = _.words(this.urlLeftPart, this.URL_CTXT);
+				this.urlLeftPart = this.urlArray[0];
+				this.urlRightPart = _(this.urlArray[1]).strRight(this.SLASH_CHAR);
+				var filter = parseInt(this.urlArray[1][0]);
+				if( _.isNaN(filter) ) 
+				{
+					this.urlRightPart = this.SLASH_CHAR + this.urlRightPart;				
+				}
+				else
+				{
+					if( _.str.include(this.urlRightPart,this.SLASH_CHAR) )
+						this.urlRightPart = this.SLASH_CHAR + this.urlRightPart;
+					else
+						this.urlRightPart = null;
+				}
+			}
+		},
+		
+		/**
+		 * Builds url for each filters in template
+		 */
+		buildUrls: function() {
+			this.filters = [];
+			
+			var self = this;
+			_.each(self.metaDataModel.getFilters(),function(f){
+				if ( !_.isNull( self.urlRightPart ) ) {
+					f.route = _.join(self.URL_CTXT, self.urlLeftPart , _(self.urlRightPart).splice(0, 0, f.id) );
+				}
+				else {
+					f.route = _.join(self.URL_CTXT, self.urlLeftPart, f.id);					
+				}
+				self.filters.push(f);
+			});
 		}
 
 	});
