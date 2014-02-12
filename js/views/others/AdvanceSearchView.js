4b9ed291816cd785c4dfb0872ce85181699b4f2c
pyfadet <pierreyves.fadet@siclic.fr>
2014-02-12 16:47:37 +0100

    refs #2250 : enregistrer les filtres avançés

--- js/views/others/AdvanceSearchView.js
+++ js/views/others/AdvanceSearchView.js
@@ -2,10 +2,12 @@ define([
 	'app',
 	'appHelpers',
 	
+	'filterModel',
+	
 	'fieldContainerView',
 	'modalSaveFilterView'
 
-], function(app, AppHelpers, FieldContainerView, ModalSaveFilterView){
+], function(app, AppHelpers, FilterModel, FieldContainerView, ModalSaveFilterView){
 
 	'use strict';
 
@@ -65,27 +67,27 @@ define([
 		/** Prepares filters for generic list view
 		*/
 		performSearch: function(e){
-
 			e.preventDefault();
-			
-			var filters = [];
-			
-			_.each(this.fieldContainerView.components, function(c, i) {
+			this.view.applyAdvancedFilters(this.getDomain());
+		},
 
+		getDomain: function() {
+			var domain = [];
+				
+			_.each(this.fieldContainerView.components, function(c, i) {
+			
 				var field = c.field.key
 				var value = c.getValue();
 				var operator = c.getOperator('key');
-
+			
 				if(!_.isNull(value)){
-					filters.push({ field: field, operator: operator, value: value });
+					domain.push({ field: field, operator: operator, value: value });
 				}
 			
 			});
-
-			this.view.applyAdvancedFilters(filters);
+			
+			return domain;
 		},
-
-
 
 
 		/** Transform JSON filter to readable String
@@ -120,11 +122,16 @@ define([
 		/** Display modal to save the Filter
 		*/
 		modalSaveFilter: function(e){
-
-			var filter = createFilter()
+			e.preventDefault();
+			var modelJSON = this.view.metaDataModel.toJSON();
+			var filter = new FilterModel({ 
+				domain	: JSON.stringify(this.getDomain()), 
+				user_id : app.current_user.getUID(),
+				model_id: modelJSON.model//parseInt(this.view.collection.modelId).toString(),				
+			});
 			app.views.modalSaveFilterView = new ModalSaveFilterView({
 				el 		: '#modalSaveFilter',
-				model 	: this.model,
+				model 	: filter,
 			});
 
 		}
