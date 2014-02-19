f5c2dc5a2182a35811354d950233bdc82a454fb4
plancher bruno <bruno.plancher@gmail.com>
2014-02-19 12:09:44 +0100

    added BookingModel#fetchFromBackend method, to compute all data in one function

--- app-reservations/js/models/BookingModel.js
+++ app-reservations/js/models/BookingModel.js
@@ -639,6 +639,34 @@ define([
 			this.lines.add(lineModel);
 			lineModel.setParentBookingModel(this);
 		},
+		
+		/**
+		 * Fetch Booking and all of its linked models, such as BookingLines or BookingRecurrence.
+		 * Set this.lines and this.recurrence attributes with their respecting *Model
+		 */
+		fetchFromBackend: function(){
+			var self = this;
+			var deferred = $.Deferred();
+			this.fetch({silent: true}).done(function(){
+				var waitDeferred = [];
+				//fetch and render lines
+				waitDeferred.push(self.fetchLines());
+				
+				//fetch and render recurrence if exists
+				if(self.getRecurrence() != false){
+					var recurrence = new BookingRecurrenceModel({id:self.getRecurrence('id')});
+					waitDeferred.push(recurrence.fetch());
+					recurrence.setTemplate(self);
+				}
+				$.when.apply($, waitDeferred).done(function(){
+					deferred.resolve();
+				}).fail(function(e){
+					deferred.reject();
+				});
+			});
+			return deferred;
+		},
+		
 		/**
 		 * Fetch BookingLines and return Deferred object. resolve deferred if all lines (and their bookable) have been fetch, reject otherwise
 		 */
