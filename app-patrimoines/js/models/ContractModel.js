da8fb2880adc112d6944ace3cc71b07263959afa
plancher bruno <bruno.plancher@gmail.com>
2014-03-04 15:00:41 +0100

    updated ContractModel and Collection to add 'state' field and to implement 'getUserActions' method

--- app-patrimoines/js/models/ContractModel.js
+++ app-patrimoines/js/models/ContractModel.js
@@ -15,7 +15,7 @@ define([
 		
 		urlRoot: '/api/openpatrimoine/contracts',
 
-		fields: ['id', 'name', 'actions', 'date_start_order', 'date_end_order', 'internal_inter', 'technical_service_id', 'supplier_id', 'provider_name', 'patrimoine_is_equipment', 'equipment_id' ,'site_id' ,'patrimoine_name'],
+		fields: ['id', 'name', 'actions', 'date_start_order', 'date_end_order', 'internal_inter', 'technical_service_id', 'supplier_id', 'provider_name', 'patrimoine_is_equipment', 'equipment_id' ,'site_id' ,'patrimoine_name', 'state'],
 
 		searchable_fields: [
 			{
@@ -28,6 +28,33 @@ define([
 			}
 			
 		],
+		
+		getUserMainAction: function(){
+			var ret = '';
+			switch(this.getAttribute('state','')){
+			case 'draft':
+				ret = 'confirm';
+				break;
+			case 'confirm':
+				ret = 'done';
+				break;
+			case 'done':
+				ret = 'extend';
+				break;
+			default:
+				ret = 'confirm';
+			}
+			return ret;
+		},
+		/**
+		 * Method used to compute actions authorized for user, and compute the mainAction to display on itemListViews
+		 */
+		getUserActions: function(){
+			var actions = this.getAttribute('actions',[]);
+			var mainAction = this.getUserMainAction();
+			return {mainAction: mainAction, otherActions: _.without(actions, mainAction)};
+		},
+		
 		//method to retrieve attribute with standard return form
 		getAttribute: function(key,default_value){
 			var val = this.get(key);
@@ -81,8 +108,43 @@ define([
 		
 			// Actions of the requests //
 		actions : {
+			update: {
+				key					: 'update',
+				icon				: 'fa-pencil',
+				translation			: app.lang.actions.update
+			},
+			delete: {
+				key					: 'delete',
+				icon				: 'fa-trash-o',
+				translation			: app.lang.actions.delete
+			},
+			
+			confirm: {
+				key					: 'confirm',
+				color				: 'success',
+				icon				: 'fa-check',
+				translation			: app.lang.actions.validate
+			},
+			
+			done: {
+				key					: 'done',
+				color				: 'default',
+				icon				: 'fa-thumbs-o-up',
+				translation			: app.lang.actions.closed
+			},
+			extend: {
+				key					: 'extend',
+				color				: 'info',
+				icon				: 'fa-pencil',
+				translation			: app.lang.actions.extendContract
+			},
+			
+			foo: {
+				key					: 'foo',
+				icon				: 'fa-ban-circle',
+				translation			: 'bar'
+			}
 
 		}
-	
 	});
 });
