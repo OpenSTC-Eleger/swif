281c7bb8b95084d04e4ed9160ed2e39675cc172d
Simon <simon.mahe@siclic.fr>
2014-03-05 11:08:51 +0100

    fixes #2552 set Special domain BookingListView

--- app-reservations/js/views/lists/BookingsListView.js
+++ app-reservations/js/views/lists/BookingsListView.js
@@ -59,6 +59,13 @@ define([
 			if(_.isUndefined(this.collection)){ this.collection = new BookingsCollection(); }
 			else{this.collection.reset();}
 
+
+
+			// Add filter for claimer to fetch only their own bookings //
+			if(!app.current_user.isResaManager()){
+				this.specialDomain = {field: 'partner_id.address', operator: 'in', value: app.current_user.get('contact_id')};
+			}
+
 			this.buttonAction = app.lang.resa.actions.addBooking;
 			GenericListView.prototype.initialize.apply(this, arguments);
 		},
@@ -81,6 +88,7 @@ define([
 			}
 			GenericListView.prototype.partialRender.apply(self);
 		},
+
 
 
 		/** Display the view
@@ -144,21 +152,15 @@ define([
 		},
 
 
+
 		/** Got to create Resa form
 		*/
 		createResa: function(e){
 			e.preventDefault();
 
 			app.router.navigate(_.strLeft(app.routes.formReservation.url, '('), {trigger: true, replace: true});
-		},
+		}
 
-		// TODO GenericListView
-		/*//add filter for claimer to fetch only their own bookings
-		if(!app.current_user.isResaManager()){
-			this.fetchParams.data.filters  = _.toArray(this.fetchParams.data.filters);
-			this.fetchParams.data.filters.push({field: 'partner_id.address.id', operator:'in', value:app.current_user.get('contact_id')});
-			this.fetchParams.data.filters = app.objectifyFilters(this.fetchParams.data.filters);
-		}*/
 
 	});
 
