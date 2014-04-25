022f7fa7ddce6338d097b1bff353a3eb1569ee83
plancher bruno <bruno.plancher@gmail.com>
2014-04-25 15:34:23 +0200

    updated GenericRecurrenceModel to manage occurrence_ids field, and to manage '/recurrence_dates' route

--- js/models/GenericRecurrenceModel.js
+++ js/models/GenericRecurrenceModel.js
@@ -21,10 +21,12 @@ define([
 	var RecurrenceModel = GenericModel.extend({
 
 
-		fields     : ['id', 'recur_periodicity', 'recur_week_monday', 'recur_week_tuesday', 'recur_week_wednesday', 'recur_week_thursday', 'recur_week_friday', 'recur_week_saturday', 'recur_week_sunday', 'recur_month_type', 'recur_month_absolute', 'recur_month_relative_weight', 'recur_month_relative_day', 'recur_type', 'date_start', 'recur_length_type','date_end'],
-
-		urlRecurrenceRoot    : '/recurrence_dates',
-
+		recurFields     : ['id', 'recur_periodicity', 'recur_week_monday', 'recur_week_tuesday', 'recur_week_wednesday', 'recur_week_thursday', 'recur_week_friday', 'recur_week_saturday', 'recur_week_sunday', 'recur_month_type', 'recur_month_absolute', 'recur_month_relative_weight', 'recur_month_relative_day', 'recur_type', 'date_start','date_end'],
+		
+		//When inheriting, override these two values to customize the resource to be fetched for recurrence
+		urlRecurrenceResource : '/api/openstc/task_recurrences',
+		urlRecurrenceFetch    : '/recurrence_dates',
+		
 		searchable_fields: [
 			{ key: '', type : '' }
 		],
@@ -35,56 +37,55 @@ define([
 
 		},
 		
-		fetchFieldMetadata: function(){
-			return $.ajax({
-				url      : this.url,
-				method   : 'HEAD',
-				dataType : 'text',
-				data     : {filters: {}},
-				success  : function(data,status,request){
-
-					self.fieldsMetadata = {};
-					
-					//Set advanced filters for collection with metadatas
-					try {
-						self.fieldsMetadata = JSON.parse(request.getResponseHeader('Model-Fields'));
-						//TODO: I keep self.collection for backward compatibility, must be refactored
-						self.collection = {};
-						self.collection.fieldsMetadata = self.fieldsMetadata;
-						_.each(self.advanced_searchable_fields, function(fieldToKeep){
-							var field = _.find(self.fieldsMetadata,function(value,key){
-								return fieldToKeep.key == key;
-							});
-							_.extend(fieldToKeep, field);
-						});
-					}
-					catch(e){
-						console.log('Meta data are not valid');
-					}
-
-					//Get model Id to obtain his filters
-					self.modelId = request.getResponseHeader('Model-Id');
-
+		addOccurrence: function(model){
+			if(model.isNew()){
+				var vals = this.getAttribute('occurrence_ids',[]);
+				vals.push([0,0,model.getSaveVals()]);
+				this.set({occurrence_ids:vals});
+			}
+		},
+		
+		removeOccurrence: function(model){
+			if(!model.isNew()){
+				var vals = this.getAttribute('occurrence_ids',[]);
+				vals.push([2,model.getId()]);
+				this.set({occurrence_ids:vals});
+			}
+		},
+		
+		removeAllOccurrences: function(){
+			var vals = [];
+			_.each(this.getAttribute('occurrence_ids',[]), function(occurrence_id){
+				//check if occurrence_id is an ID or is already a deletion action (not a good way to do, refacto this soon)
+				if(_.isNumber(occurrence_id)){
+					vals.push([2,occurrence_id]);
 				}
-
 			});
+			if(vals.length > 0){
+				this.set({occurrence_ids:vals});
+			}
 		},
 		
 		fetchDates: function(){
 			var self = this;
-			var deferred = $.Deferred;
-			return this.saveToBackend().done(function(){
-				$.ajax({
-					method	: 'GET',
-					url		: self.urlRoot + "/" + self.getId()  + self.urlRecurrenceRoot,
-					success : function(){
-						self.fetch().done(function(){
-							self.trigger('recurrence', self);
-						})
-					},
+			var data = this.getSaveVals();
+			data.id = null;
+			var recurModel = new GenericModel(data);
+			recurModel.urlRoot = this.urlRecurrenceResource;
+			recurModel.fetchMetadata().then(function(){
+				recurModel.save().done(function(data){
+					recurModel.set({id:data});
+					$.ajax({
+						method	: 'GET',
+						url		: self.urlRecurrenceResource + "/" + recurModel.getId()  + self.urlRecurrenceFetch,
+						success : function(data){
+								self.removeAllOccurrences();
+								self.trigger('recurrence', data);
+							}
+					});
 				});
 			});
-		},
+		}
 	
 	});
 
