35d3325b39e04d2e017813125443f79a02a10d60
pyfadet <pierreyves.fadet@siclic.fr>
2014-05-07 11:32:04 +0200

    refs #2821 : information des coût sur une intervention

--- app-interventions/js/collections/InterventionsCollection.js
+++ app-interventions/js/collections/InterventionsCollection.js
@@ -23,7 +23,7 @@ define([
 
 		url    : '/api/openstc/interventions',
 
-		fields : ['id', 'name', 'description', 'tasks', 'state', 'service_id', 'site1', 'site_details', 'date_deadline', 'planned_hours', 'effective_hours', 'total_hours', 'tooltip', 'progress_rate', 'overPourcent', 'actions','create_uid', 'create_date', 'ask_id', 'equipment_id', 'has_equipment', 'cost'],
+		fields : ['id', 'name', 'description', 'tasks', 'state', 'service_id', 'site1', 'site_details', 'date_deadline', 'planned_hours', 'effective_hours', 'total_hours', 'tooltip', 'progress_rate', 'overPourcent', 'actions','create_uid', 'create_date', 'ask_id', 'equipment_id', 'has_equipment', 'cost', 'hr_cost', 'equipment_cost', 'consumable_cost'],
 
 		advanced_searchable_fields: [
 			{ key: 'site1',         label: app.lang.place },
